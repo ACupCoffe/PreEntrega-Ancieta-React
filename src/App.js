216c7690b2f359212a7a9c1c592b8484eb44c70e
Ancieta <ancietalea.imp@gmail.com>
2023-12-12 22:12:38 -0300

    Solucion errores

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import NavBar from "./components/header/NavBar";
 import ContainerCardItems from "./components/componentes_item/ContainerCardItems";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailsItem from "./components/componentes_item/ItemDetail";
-import { createContext, useState } from "react";
 import ProviderContextoListCart from "./components/componentes_item/InformationCarrito";
 import DetalleCompras from "./components/componentes_item/DetalleDeCompras"; // Importa el componente Brief
 
